30eef9d2f18a2663b3265b9725316bd42eea05af
Fooxii <ironata07@gmail.com>
2025-12-14 15:29:09 -0400

    Added bubble sort script and a file with its big O notations

---
+++ 0-bibble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function that sorts an array of intergers in ascending order
+ * using the Buble Sort algorithm
+ * @array: array of intergers
+ * @size: size of array
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+size_t i;
+int temp;
+int swapped = 1;
+
+if (!array || size < 2)
+return;
+
+while (swapped == 1)
+{
+swapped = 0;
+i = 0;
+
+while (i < size - 1)
+{
+if (array[i] > array[i + 1])
+{
+temp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = temp;
+
+swapped = 1;
+print_array(array, size);
+}
+i++;
+}
+}
+}
