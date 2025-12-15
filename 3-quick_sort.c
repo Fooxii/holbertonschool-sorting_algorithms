d4358b6841a75914fc531c9a78d924e10f19a555
Fooxii <ironata07@gmail.com>
2025-12-14 21:20:23 -0400

    Added script that separates the array into two sections

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,49 @@
 #include "sort.h"
 
 /**
+ * partition - function that partitions the array
+ * @array: array of integers
+ * @low: starting index of the array
+ * @high: ending index of the array
+ * @size: size of the array
+ * Return: pivot index
+ */
+
+int partition(int *array, int low, int high, size_t size)
+{
+int pivot, temp;
+int i, j;
+
+pivot = array[high];
+i = low - 1;
+
+for (j = low; j < high; j++)
+{
+if (array[j] < pivot)
+{
+i++;
+if (i != j)
+{
+temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+print_array(array, size);
+}
+}
+}
+
+if (i + 1 != high)
+{
+temp = array[i + 1];
+array[i + 1] = array[high];
+array[high] = temp;
+print_array(array, size);
+}
+
+return (i + 1);
+}
+
+/**
  * quick_sort_recursive - function that implements the Quick sort algorithm
  * @array: array of integers
  * @low: starting index of the array
